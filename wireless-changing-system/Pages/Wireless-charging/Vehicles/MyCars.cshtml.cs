9e69be7c501758e85d09128af7b22be837c4326c
namspidey <141756920+namspidey@users.noreply.github.com>
2025-02-25 14:54:43 +0700

    add bootstrap

---
+++ wireless-changing-system/Pages/Wireless-charging/Vehicles/MyCars.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wireless_changing_system.Pages.Wireless_charging.Vehicles
+{
+    public class MyCarsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
